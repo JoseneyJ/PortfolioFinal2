19c15f81cf02679779f451945f78409660442d49
JoseneyJ <119374215+JoseneyJ@users.noreply.github.com>
2025-10-14 10:54:45 -0400

    footer fixed

--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -4,7 +4,7 @@ import './footer.css'
 const Footer = () => {
   return (
     <footer className= "footer">
-        Copyright &#169; 2025 Joseney Jean Pierre. All rights received.
+        Copyright &#169; 2025 Joseney Jean Pierre. All rights reserved.
     </footer>
   );
 }
