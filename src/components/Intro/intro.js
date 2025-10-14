2b7688ea025808f3350f7ac6b6274deb00044479
JoseneyJ <119374215+JoseneyJ@users.noreply.github.com>
2025-10-14 10:41:24 -0400

    intro update, banner added, fade in

--- src/components/Intro/intro.js
+++ src/components/Intro/intro.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import './intro.css';
-import bg from '../../assests/image.png';
+import bg from '../../assests/image2.png';
 
 
 
 const Intro = () => {
   return (
-   <section id="intro">
+    
+    <section id="intro">
+    <div className='profile'>
+    <img src={bg} alt='Profile' className="bg"/>
+    </div>
     <div className="introContent">
         <span className="hello">Greetings!</span>
         <span className="introText"> I'm <span className= "introName">Joseney</span> <br />Software Developer</span>
-        <p className="introPara">I am a motivated Software Developer with a passion for understanding user experience. </p>
+        <p className="introPara">I am a highly motivated, results-driven professional with a passion for understanding and enhancing user experience. </p>
         <a href= "JoseneyJ. Resume.pdf" download="JoseneyJ. Resume.pdf">
          <button className="nav__button">My Resume</button>
         </a>
 
     </div>
-    <img src={bg} alt="Profile" className="bg"/>
+  
    </section>
   );
 }
